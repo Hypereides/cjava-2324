3152f1088cbb1c7043c338bf1df72bb05f209aa9
Pete <121137355+Hypereides@users.noreply.github.com>
2023-12-19 20:38:59 +0200

    New version

--- src/c/week8/narcissisticnumber.c
+++ src/c/week8/narcissisticnumber.c
@@ -1,8 +1,9 @@
 /*The following code generates all possible 4 digit armstrong numbers ( numbers whose each digit respectively in the power of 
 n where n == to the number length is equal to the digit it self )
 
-Basic for loop including a while loop fetches a number from 1000 = 9999 and compares the sum of each digit to the overall sum of the number 
-
+Basic, for loop including a while loop fetches a number from 1000 = 9999 and compares the sum of each digit to the overall sum of the number 
+ ******************************** Right bellow the initial code , within comments i have also
+  included a different version that executes way faster 
 */
 
 
@@ -29,3 +30,39 @@ int main()
         }
     }
 }
+
+
+//Written below is a version that increases the execution speed
+
+
+
+/*int main()
+{
+    int n, nc, sum;
+    int powers[10];
+
+    //Given the power of numbers is always going to be 4 , we can simply store the fourth power of 1-9
+    for(int i = 0; i < 10; i++) {
+        powers[i] = i * i * i * i;
+    }
+
+    for(n = 1000; n <= 9999; n++) {
+        nc = n;
+        sum = 0;
+        while (nc != 0)
+        {
+            int d = nc % 10;
+            sum += powers[d];
+            if(sum > n) {
+                break; //if n is smaller than sum there is no need to continue the loop since there is no possible way for the number to be an armstrong one
+            }
+            nc /= 10;
+        }
+        
+        if(n == sum)
+        {
+            printf("Armstrong number generated: %d\n", n);
+        }
+    }
+
+*/
